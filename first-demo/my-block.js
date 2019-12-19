d762315f09fd9d3e645fa39355c5bcb9f6f733f2
Ronak Vanpariya <ronak@creolestudios.com>
2019-12-19 19:29:55 +0530

    added the first demo

---
+++ first-demo/my-block.js
@@ -0,0 +1,45 @@
+
+
+wp.blocks.registerBlockType('guten/guten-box',{
+    title: 'my cool Border box',
+    icon: 'smiley',
+    category: 'common',
+    attributes: {
+        content:{type: 'string'},
+        color: {type: 'string'}
+    },
+    edit: function( props ){
+
+        function updateContent( event ){
+            props.setAttributes({content: event.target.value})
+        }
+        function updateColor( value ){
+            props.setAttributes( { color:value.hex } )
+        }
+
+        return wp.element.createElement(
+            'div',
+            null,
+            wp.element.createElement(
+                'h3',
+                {style: { color: props.attributes.color } },
+                'your cooll border box'
+            ),
+            React.createElement('input', {type: 'text' , value: props.attributes.content , onChange: updateContent}),
+            React.createElement(wp.components.ColorPicker, { color: props.attributes.color, onChangeComplete: updateColor }),
+        );
+    },
+    save: function( props ){
+        
+        return wp.element.createElement(
+            'div',
+            null,
+            wp.element.createElement(
+                'h3',
+                {style: { color: props.attributes.color } },
+                'your cooll border box'
+            ),
+            React.createElement('input', {type: 'text', value: props.attributes.content})
+        );
+    },
+})
--- plugin.php
+++ plugin.php
@@ -1,16 +1,19 @@
- <?php
+<?php
 /**
  * Plugin Name: Guten Blog Demo
  * Author: Ronak vanpariya (vanpariyar@gmail.com)
+ * Plugin URI: https://github.com/vanpariyar/gutenberg-blocks-plugin
+ * Author URI: https://vanpariyar.github.io
  * Version: 1.0.0
  */
 function loadMyBlockFiles() {
     wp_enqueue_script(
       'my-super-unique-handle',
-      plugin_dir_url(__FILE__) . 'my-block.js',
+      plugin_dir_url(__FILE__) . '/first-demo/my-block.js',
       array('wp-blocks', 'wp-i18n', 'wp-editor'),
       true
     );
   }
-   
   add_action('enqueue_block_editor_assets', 'loadMyBlockFiles');
+
+?>
