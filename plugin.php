074bd94b625cdf8eba5a72c6d078175768493aad
Ronak J Vanpariya <vanpariyar@gmail.com>
2019-12-19 18:10:42 +0530

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,16 @@
+ <?php
+/**
+ * Plugin Name: Guten Blog Demo
+ * Author: Ronak vanpariya (vanpariyar@gmail.com)
+ * Version: 1.0.0
+ */
+function loadMyBlockFiles() {
+    wp_enqueue_script(
+      'my-super-unique-handle',
+      plugin_dir_url(__FILE__) . 'my-block.js',
+      array('wp-blocks', 'wp-i18n', 'wp-editor'),
+      true
+    );
+  }
+   
+  add_action('enqueue_block_editor_assets', 'loadMyBlockFiles');
